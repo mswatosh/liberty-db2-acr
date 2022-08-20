e721da792220c959dbe1191e4e85fc07a7ce58fd
Mark Swatosh <mark.swatosh@gmail.com>
2022-08-19 23:37:57 -0500

    Adding Db2 and URL info to Servlet

---
+++ db2/docker-entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/bin/sh -c /var/db2_setup/lib/setup_db2_instance.sh
--- src/main/java/com/example/ExampleServlet.java
+++ src/main/java/com/example/ExampleServlet.java
@@ -2,18 +2,24 @@ package com.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
-
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 @WebServlet("/example")
 public class ExampleServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+
+    @Resource(name = "jdbc/db2ds")
+    DataSource ds;
 
     @Override
     protected void doGet(HttpServletRequest request,
@@ -25,6 +31,17 @@ public class ExampleServlet extends HttpServlet {
 
 
         pw.println("<h2>Example Servlet</h2>"); 
+
+        try {
+            DatabaseMetaData dbmd = ds.getConnection().getMetaData();
+            pw.println("URL:" + dbmd.getURL());
+        } catch (SQLException e) {
+            pw.println("SQLState: " + e.getSQLState());
+            pw.println("SQLCode: " + e.getErrorCode());
+        }
+        
+
+
     
    }
 } 
